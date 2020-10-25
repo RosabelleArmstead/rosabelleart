61b9d660a13f1ab5860fb5f298e2b4a56f113607
Rosabelle Armstead <rosabelle.l.armstead@gmail.com>
2020-10-25 18:49:32 +0000

    add data files

---
+++ json/gallery.js
@@ -0,0 +1,300 @@
+art = `[
+    { 
+        "gallery": [
+			{
+			  "name": "Vista",
+			  "media": "procreate",
+			  "filename": "PrintVista.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Ellie",
+			  "media": "procreate",
+			  "filename": "PrintEllie.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Bear Sketch",
+			  "media": "procreate",
+			  "filename": "PrintBearSketch.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Root Sketch",
+			  "media": "procreate",
+			  "filename": "PrintRootSketch.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Sameen Shaw Sketch",
+			  "media": "procreate",
+			  "filename": "PrintSameenShawSketch.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Harold Finch Sketch",
+			  "media": "procreate",
+			  "filename": "PrintHaroldFinchSketch.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "John Reese Sketch",
+			  "media": "procreate",
+			  "filename": "PrintJohnReeseSketch.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Round of Robins",
+			  "media": "procreate",
+			  "filename": "PrintRobins.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "European Robin",
+			  "media": "procreate",
+			  "filename": "PrintEuropeanRobin.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Oriental Magpie Robin",
+			  "media": "procreate",
+			  "filename": "PrintOrientalMagpieRobin.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Red-capped Robin",
+			  "media": "procreate",
+			  "filename": "PrintRedCappedRobin.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Eastern Yellow Robin",
+			  "media": "procreate",
+			  "filename": "PrintYellowRobin.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Pink Robin",
+			  "media": "procreate",
+			  "filename": "PrintPinkRobin.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Niagara Falls",
+			  "media": "procreate",
+			  "filename": "PrintNiagaraFalls.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Wax Resist Experiment",
+			  "media": "procreate",
+			  "filename": "PrintWaxResist.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Root (II)",
+			  "media": "adobe photoshop cs6",
+			  "filename": "PrintRoot2.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Harold Finch",
+			  "media": "adobe photoshop cs6",
+			  "filename": "PrintFinch.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Root",
+			  "media": "adobe photoshop cs6",
+			  "filename": "PrintRoot.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Sameen Shaw",
+			  "media": "adobe photoshop cs6",
+			  "filename": "PrintShaw.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Lincoln",
+			  "media": "adobe photoshop cs6",
+			  "filename": "PrintLincoln.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Lexa (II)",
+			  "media": "adobe photoshop cs6",
+			  "filename": "PrintLexa.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Clarke Griffin",
+			  "media": "adobe photoshop cs6",
+			  "filename": "PrintClarke.png",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Chris Pratt",
+			  "media": "HB, 3B and 7B pencils",
+			  "filename": "ChrisPratt.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Chris Colfer",
+			  "media": "paint.NET",
+			  "filename": "ChrisColfer.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Anna Kendrick",
+			  "media": "HB, 2B, 3B and 6B pencils",
+			  "filename": "AnnaKendrick.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Joey Graceffa",
+			  "media": "HB, 3B and 7B pencils",
+			  "filename": "JoeyGraceffa.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Lexa",
+			  "media": "HB, 2B and 4B pencils",
+			  "filename": "Lexa.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Heather Morris",
+			  "media": "HB, 3B and 7B pencils",
+			  "filename": "HeatherMorris.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Taylor Swift",
+			  "media": "HB, 3B and 8B pencils",
+			  "filename": "TaylorSwift.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Jeff Blim",
+			  "media": "HB, B and 3B pencils",
+			  "filename": "JeffBlim.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Grace Helbig",
+			  "media": "HB, B and 4B pencils",
+			  "filename": "GraceHelbig.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Killian Jones",
+			  "media": "HB, 3B and 8B pencils",
+			  "filename": "Hook.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Joe Moses",
+			  "media": "graphite pencils",
+			  "filename": "JoeMoses.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Klaine (Auditorium)",
+			  "media": "graphite pencils",
+			  "filename": "KlaineAuditorium.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Becca Tobin",
+			  "media": "graphite pencils",
+			  "filename": "BeccaTobin.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Brittana (100)",
+			  "media": "graphite pencils",
+			  "filename": "Brittana100.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Joey Richter",
+			  "media": "blue colouring pencils",
+			  "filename": "JoeyRichter.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Joe Walker",
+			  "media": "colouring pencils",
+			  "filename": "JoeWalker.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Elsa",
+			  "media": "graphite pencils",
+			  "filename": "Elsa.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Darren Criss",
+			  "media": "pink and purple colouring pencils",
+			  "filename": "DarrenCriss.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "St. Paul's Cathedral",
+			  "media": "biro",
+			  "filename": "StPauls.jpg",
+			  "purchase-link": ""
+			},
+			{
+			  "name": "Westminster Abbey",
+			  "media": "watercolours",
+			  "filename": "WestminsterAbbey.jpg",
+			  "purchase-link": ""
+			}
+		  ],
+        "sketchbook": [
+            {
+              "name": "Broken Gas Mask Ellie",
+              "media": "graphite pencils",
+              "filename": "EllieGasMask.jpg"
+            },
+            {
+              "name": "Dina Study",
+              "media": "graphite pencils",
+              "filename": "DinaStudy.jpg"
+            },
+            {
+              "name": "Dog Study",
+              "media": "graphite pencils",
+              "filename": "DogStudy.jpg"
+            },
+            {
+              "name": "Ellie Study",
+              "media": "graphite pencils",
+              "filename": "EllieStudy.jpg"
+            },
+            {
+              "name": "Bears Study",
+              "media": "graphite pencils",
+              "filename": "bears.jpg"
+            },
+            {
+              "name": "Birds Study (II)",
+              "media": "graphite pencils",
+              "filename": "birds2.jpg"
+            },
+            {
+              "name": "Birds Study (I)",
+              "media": "graphite pencils",
+              "filename": "birds1.jpg"
+            },
+            {
+              "name": "Clicker",
+              "media": "graphite pencils",
+              "filename": "Clicker.jpg"
+            }
+        ]
+    }
+]`;
