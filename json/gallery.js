5b06e1c369336d35de238ab56c3d566e737e7ef1
Rosabelle Armstead <rosabelle.l.armstead@gmail.com>
2020-10-28 02:19:53 +0000

    add modal display to gallery

--- json/gallery.js
+++ json/gallery.js
@@ -5,296 +5,346 @@ art = `[
 			  "name": "Vista",
 			  "media": "procreate",
 			  "filename": "PrintVista.png",
-			  "purchase": "yes"
+			  "purchase": "yes",
+			  "id": "vistaid"
 			},
 			{
 			  "name": "Ellie",
 			  "media": "procreate",
 			  "filename": "PrintEllie.png",
-			  "purchase": "yes"
+			  "purchase": "yes",
+			  "id": "ellieid"
 			},
 			{
 			  "name": "Bear Sketch",
 			  "media": "procreate",
 			  "filename": "PrintBearSketch.png",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "bearid"
 			},
 			{
 			  "name": "Root Sketch",
 			  "media": "procreate",
 			  "filename": "PrintRootSketch.png",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "rootid"
 			},
 			{
 			  "name": "Sameen Shaw Sketch",
 			  "media": "procreate",
 			  "filename": "PrintSameenShawSketch.png",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "shawid"
 			},
 			{
 			  "name": "Harold Finch Sketch",
 			  "media": "procreate",
 			  "filename": "PrintHaroldFinchSketch.png",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "finchid"
 			},
 			{
 			  "name": "John Reese Sketch",
 			  "media": "procreate",
 			  "filename": "PrintJohnReeseSketch.png",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "reeseid"
 			},
 			{
 			  "name": "Round of Robins",
 			  "media": "procreate",
 			  "filename": "PrintRobins.png",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "robinsid"
 			},
 			{
 			  "name": "European Robin",
 			  "media": "procreate",
 			  "filename": "PrintEuropeanRobin.png",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "eurobinid"
 			},
 			{
 			  "name": "Oriental Magpie Robin",
 			  "media": "procreate",
 			  "filename": "PrintOrientalMagpieRobin.png",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "omrobinid"
 			},
 			{
 			  "name": "Red-capped Robin",
 			  "media": "procreate",
 			  "filename": "PrintRedCappedRobin.png",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "rcrobinid"
 			},
 			{
 			  "name": "Eastern Yellow Robin",
 			  "media": "procreate",
 			  "filename": "PrintYellowRobin.png",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "yrobinid"
 			},
 			{
 			  "name": "Pink Robin",
 			  "media": "procreate",
 			  "filename": "PrintPinkRobin.png",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "probinid"
 			},
 			{
 			  "name": "Niagara Falls",
 			  "media": "procreate",
 			  "filename": "PrintNiagaraFalls.png",
-			  "purchase": "yes"
+			  "purchase": "yes",
+			  "id": "niagarafallsid"
 			},
 			{
 			  "name": "Wax Resist Experiment",
 			  "media": "procreate",
 			  "filename": "PrintWaxResist.png",
-			  "purchase": "yes"
+			  "purchase": "yes",
+			  "id": "waxresistid"
 			},
 			{
 			  "name": "Root (II)",
 			  "media": "adobe photoshop cs6",
 			  "filename": "PrintRoot2.jpg",
-			  "purchase": "yes"
+			  "purchase": "yes",
+			  "id": "rootiiid"
 			},
 			{
 			  "name": "Harold Finch",
 			  "media": "adobe photoshop cs6",
 			  "filename": "PrintFinch.png",
-			  "purchase": "yes"
+			  "purchase": "yes",
+			  "id": "finchiiid"
 			},
 			{
 			  "name": "Root",
 			  "media": "adobe photoshop cs6",
 			  "filename": "PrintRoot.png",
-			  "purchase": "yes"
+			  "purchase": "yes",
+			  "id": "rootiiiid"
 			},
 			{
 			  "name": "Sameen Shaw",
 			  "media": "adobe photoshop cs6",
 			  "filename": "PrintShaw.png",
-			  "purchase": "yes"
+			  "purchase": "yes",
+			  "id": "shawiiid"
 			},
 			{
 			  "name": "Lincoln",
 			  "media": "adobe photoshop cs6",
 			  "filename": "PrintLincoln.png",
-			  "purchase": "yes"
+			  "purchase": "yes",
+			  "id": "lincolnid"
 			},
 			{
 			  "name": "Lexa (II)",
 			  "media": "adobe photoshop cs6",
 			  "filename": "PrintLexa.png",
-			  "purchase": "yes"
+			  "purchase": "yes",
+			  "id": "lexaiiid"
 			},
 			{
 			  "name": "Clarke Griffin",
 			  "media": "adobe photoshop cs6",
 			  "filename": "PrintClarke.png",
-			  "purchase": "yes"
+			  "purchase": "yes",
+			  "id": "clarkeid"
 			},
 			{
 			  "name": "Chris Pratt",
 			  "media": "HB, 3B and 7B pencils",
 			  "filename": "ChrisPratt.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "chrisprattid"
 			},
 			{
 			  "name": "Chris Colfer",
 			  "media": "paint.NET",
 			  "filename": "ChrisColfer.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "chriscolferid"
 			},
 			{
 			  "name": "Anna Kendrick",
 			  "media": "HB, 2B, 3B and 6B pencils",
 			  "filename": "AnnaKendrick.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "akendrickid"
 			},
 			{
 			  "name": "Joey Graceffa",
 			  "media": "HB, 3B and 7B pencils",
 			  "filename": "JoeyGraceffa.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "jgraceffaid"
 			},
 			{
 			  "name": "Lexa",
 			  "media": "HB, 2B and 4B pencils",
 			  "filename": "Lexa.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "lexaid"
 			},
 			{
 			  "name": "Heather Morris",
 			  "media": "HB, 3B and 7B pencils",
 			  "filename": "HeatherMorris.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "hmorrisid"
 			},
 			{
 			  "name": "Taylor Swift",
 			  "media": "HB, 3B and 8B pencils",
 			  "filename": "TaylorSwift.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "tswiftid"
 			},
 			{
 			  "name": "Jeff Blim",
 			  "media": "HB, B and 3B pencils",
 			  "filename": "JeffBlim.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "jblimid"
 			},
 			{
 			  "name": "Grace Helbig",
 			  "media": "HB, B and 4B pencils",
 			  "filename": "GraceHelbig.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "ghelbigid"
 			},
 			{
 			  "name": "Killian Jones",
 			  "media": "HB, 3B and 8B pencils",
 			  "filename": "Hook.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "kjonesid"
 			},
 			{
 			  "name": "Joe Moses",
 			  "media": "graphite pencils",
 			  "filename": "JoeMoses.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "jmosesid"
 			},
 			{
 			  "name": "Klaine (Auditorium)",
 			  "media": "graphite pencils",
 			  "filename": "KlaineAuditorium.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "klaineid"
 			},
 			{
 			  "name": "Becca Tobin",
 			  "media": "graphite pencils",
 			  "filename": "BeccaTobin.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "btobinid"
 			},
 			{
 			  "name": "Brittana (100)",
 			  "media": "graphite pencils",
 			  "filename": "Brittana100.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "brittanaid"
 			},
 			{
 			  "name": "Joey Richter",
 			  "media": "blue colouring pencils",
 			  "filename": "JoeyRichter.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "jrichterid"
 			},
 			{
 			  "name": "Joe Walker",
 			  "media": "colouring pencils",
 			  "filename": "JoeWalker.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "jwalkerid"
 			},
 			{
 			  "name": "Elsa",
 			  "media": "graphite pencils",
 			  "filename": "Elsa.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "elsaid"
 			},
 			{
 			  "name": "Darren Criss",
 			  "media": "pink and purple colouring pencils",
 			  "filename": "DarrenCriss.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "dcrissid"
 			},
 			{
 			  "name": "St. Paul's Cathedral",
 			  "media": "biro",
 			  "filename": "StPauls.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "stpaulsid"
 			},
 			{
 			  "name": "Westminster Abbey",
 			  "media": "watercolours",
 			  "filename": "WestminsterAbbey.jpg",
-			  "purchase": ""
+			  "purchase": "",
+			  "id": "westminsterid"
 			}
-		  ],
+		],
         "sketchbook": [
-            {
-              "name": "Broken Gas Mask Ellie",
-              "media": "graphite pencils",
-              "filename": "EllieGasMask.jpg"
-            },
-            {
-              "name": "Dina Study",
-              "media": "graphite pencils",
-              "filename": "DinaStudy.jpg"
-            },
-            {
-              "name": "Dog Study",
-              "media": "graphite pencils",
-              "filename": "DogStudy.jpg"
-            },
-            {
-              "name": "Ellie Study",
-              "media": "graphite pencils",
-              "filename": "EllieStudy.jpg"
-            },
-            {
-              "name": "Bears Study",
-              "media": "graphite pencils",
-              "filename": "bears.jpg"
-            },
-            {
-              "name": "Birds Study (II)",
-              "media": "graphite pencils",
-              "filename": "birds2.jpg"
-            },
-            {
-              "name": "Birds Study (I)",
-              "media": "graphite pencils",
-              "filename": "birds1.jpg"
-            },
-            {
-              "name": "Clicker",
-              "media": "graphite pencils",
-              "filename": "Clicker.jpg"
-            }
-        ]
+			{
+			  "name": "Broken Gas Mask Ellie",
+			  "media": "graphite pencils",
+			  "filename": "EllieGasMask.jpg",
+			  "id": "gasmaskid"
+			},
+			{
+			  "name": "Dina Study",
+			  "media": "graphite pencils",
+			  "filename": "DinaStudy.jpg",
+			  "id": "dinaid"
+			},
+			{
+			  "name": "Dog Study",
+			  "media": "graphite pencils",
+			  "filename": "DogStudy.jpg",
+			  "id": "dogiiid"
+			},
+			{
+			  "name": "Ellie Study",
+			  "media": "graphite pencils",
+			  "filename": "EllieStudy.jpg",
+			  "id": "ellieid"
+			},
+			{
+			  "name": "Bears Study",
+			  "media": "graphite pencils",
+			  "filename": "bears.jpg",
+			  "id": "bearsiiid"
+			},
+			{
+			  "name": "Birds Study (II)",
+			  "media": "graphite pencils",
+			  "filename": "birds2.jpg",
+			  "id": "birdiiid"
+			},
+			{
+			  "name": "Birds Study (I)",
+			  "media": "graphite pencils",
+			  "filename": "birds1.jpg",
+			  "id": "birdsid"
+			},
+			{
+			  "name": "Clicker",
+			  "media": "graphite pencils",
+			  "filename": "Clicker.jpg",
+			  "id": "clickerid"
+			}
+		]
     }
 ]`;
